bcfdb6401e9785dbfdd30f08cd42e455f8fec59c
김명욱 <anko@gsitm.com>
2022-06-04 23:58:34 +0900

    to rest

--- vue-news/src/main.js
+++ vue-news/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import { router } from './routes/index.js';
 
-// Vue.config.productionTip = false;
+// Vue.config.productionTip = false;;
 
 new Vue({
   render: (h) => h(App),
