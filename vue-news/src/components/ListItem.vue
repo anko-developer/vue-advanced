57b23425df8b37352e8d046418aa2da0bc6a603e
김명욱 <anko@gsitm.com>
2022-07-04 15:20:45 +0900

    복습 중

--- vue-news/src/components/ListItem.vue
+++ vue-news/src/components/ListItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <ul class="news-list">
-      <li v-for="item in listItems" class="post">
+      <li v-for="item in listItems" :key="item.id" class="post">
         <!-- 포인트 영역 -->
         <div class="points">
           {{ item.points || 0 }}
