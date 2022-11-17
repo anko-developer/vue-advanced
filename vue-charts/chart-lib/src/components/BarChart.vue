a15b002d6afff118556402898a5c917505bafae0
김명욱 <anko@gsitm.com>
2022-11-17 17:30:06 +0900

    외부 lib 모듈화

---
+++ vue-charts/chart-lib/src/components/BarChart.vue
@@ -0,0 +1,30 @@
+<template>
+  <canvas ref="barChart" id="barChart" width="400" height="400"></canvas>
+</template>
+
+<script>
+export default {
+  props: ['propsdata'],
+  // 컴포넌트 속성 && 인스턴스 속성
+  mounted() {
+    const myChart = new this.$_Chart(this.$refs.barChart, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: this.propsdata
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-charts/chart-lib/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import ChartPlugin from './plugins/ChartPlugin'
+
+Vue.config.productionTip = false
+Vue.use(ChartPlugin);
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
